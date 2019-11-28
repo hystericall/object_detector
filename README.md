b91c9e89f93d5aa883e6135a0bd1d8a4463bb953
Bui Thanh Son <buison559@gmail.com>
2019-11-28 15:32:49 +0700

    Fix minors

--- README.md
+++ README.md
@@ -1,17 +1,22 @@
 # object_detector
-A work form pyimagesearch blog
 # Installation
 *Require python 3.6*
 ```
 sudo apt-get install python3-pip # Install pip
-pip3 install opencv-contrib-python numpy imutils # Install relevant packages
+pip3 install opencv-contrib-python numpy imutils schedule flask # Install relevant packages
 ```
 # Running
 Image detector
 ```
-python3 deep_learning_object_detection.py --image images/example_01.jpg --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
+python3 deep_learning_object_detection.py --image images/example_01.jpg --pbtxt output_v2.pbtxt --model frozen_inference_graph_v2_40000.pb
 ```
 Real-time detector
 ```
-python3 real_time_object_detection.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
+python3 real_time_object_detection.py --pbtxt output_v2.pbtxt --model frozen_inference_graph_v2_40000.pb
 ```
+
+Local website application:
+```
+python3 python webstreaming.py --ip 0.0.0.0 --port 8000
+```
+Go to 0.0.0.0:8000 to see the live-streaming or 0.0.0.0:8000/upload/ to upload images
--- deep_learning_object_detection.py
+++ deep_learning_object_detection.py
@@ -9,16 +9,16 @@ import os
 import time
 
 # construct the argument parse and parse the arguments
-# ap = argparse.ArgumentParser()
-# ap.add_argument("-i", "--image", required=True,
-#   help="path to input image")
-# ap.add_argument("-p", "--prototxt", required=True,
-#   help="path to Caffe 'deploy' prototxt file")
-# ap.add_argument("-m", "--model", required=True,
-#   help="path to Caffe pre-trained model")
-# ap.add_argument("-c", "--confidence", type=float, default=0.2,
-#   help="minimum probability to filter weak detections")
-# args = vars(ap.parse_args())
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+  help="path to input image")
+ap.add_argument("-p", "--pbtxt", required=True,
+  help="path to Tensorflow 'deploy' pbtxt file")
+ap.add_argument("-m", "--model", required=True,
+  help="path to Tensorflow pre-trained model .pb file")
+ap.add_argument("-c", "--confidence", type=float, default=0.2,
+  help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
 
 # initialize the list of class labels MobileNet SSD was trained to
 # detect, then generate a set of bounding box colors for each class
@@ -27,54 +27,46 @@ COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 4))
 
 # load our serialized model from disk
 print("[INFO] loading model...")
-# net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+net = cv2.dnn.readNetFromTensorflow(args["model"], args["pbtxt"])
 # net = cv2.dnn.readNetFromTensorflow("frozen_inference_graph_v2_40000.pb", "output_v2.pbtxt")
-net = cv2.dnn.readNetFromTensorflow("frozen_inference_graph_v3_59288.pb", "output_v3.pbtxt")
 
 # load the input image and construct an input blob for the image
 # by resizing to a fixed 300x300 pixels and then normalizing it
 # (note: normalization is done via the authors of the MobileNet SSD
 # implementation)
-time_avg = 0
-for i in range(3000):
-  start = time.time()
-  image = cv2.imread("images/image_001.jpg")
-  (h, w) = image.shape[:2]
+image = cv2.imread(args["image"])
+(h, w) = image.shape[:2]
 
-  # image = cv2.imread('images/image_051.jpg')
-  # blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5)
-  blob = cv2.dnn.blobFromImage(image, size=(300, 300), swapRB=True, crop=False)
+blob = cv2.dnn.blobFromImage(image, size=(300, 300), swapRB=True, crop=False)
 
-  # pass the blob through the network and obtain the detections and
-  # predictions
-  # print("[INFO] computing object detections...")
-  net.setInput(blob)
-  detections = net.forward()
-  # loop over the detections
-  for i in np.arange(0, detections.shape[2]):
-    # extract the confidence (i.e., probability) associated with the
-    # prediction
-    confidence = detections[0, 0, i, 2]
+# pass the blob through the network and obtain the detections and
+# predictions
+print("[INFO] computing object detections...")
+net.setInput(blob)
+detections = net.forward()
+# loop over the detections
+for i in np.arange(0, detections.shape[2]):
+  # extract the confidence (i.e., probability) associated with the
+  # prediction
+  confidence = detections[0, 0, i, 2]
 
-    # filter out weak detections by ensuring the `confidence` is
-    # greater than the minimum confidence
-    if confidence > 0.5:
-      # extract the index of the class label from the `detections`,
-      # then compute the (x, y)-coordinates of the bounding box for
-      # the object
-      idx = int(detections[0, 0, i, 1])
-      box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-      (startX, startY, endX, endY) = box.astype("int")
+  # filter out weak detections by ensuring the `confidence` is
+  # greater than the minimum confidence
+  if confidence > 0.5:
+    # extract the index of the class label from the `detections`,
+    # then compute the (x, y)-coordinates of the bounding box for
+    # the object
+    idx = int(detections[0, 0, i, 1])
+    box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+    (startX, startY, endX, endY) = box.astype("int")
 
-      # display the prediction
-      label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
-      # print("[INFO] {}".format(label))
-      cv2.rectangle(image, (startX, startY), (endX, endY),
-        COLORS[idx], 2)
-      y = startY - 15 if startY - 15 > 15 else startY + 15
-      cv2.putText(image, label, (startX, y),
-        cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
-  time_avg = time_avg + time.time() - start
-print(time_avg / 3000)
-# cv2.imshow("Output", image)
-# cv2.imwrite('static/tmp/output3.jpg', image)
+    # display the prediction
+    label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
+    print("[INFO] {}".format(label))
+    cv2.rectangle(image, (startX, startY), (endX, endY),
+      COLORS[idx], 2)
+    y = startY - 15 if startY - 15 > 15 else startY + 15
+    cv2.putText(image, label, (startX, y),
+      cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+cv2.imshow("Output", image)
+cv2.imwrite('static/tmp/output3.jpg', image)
