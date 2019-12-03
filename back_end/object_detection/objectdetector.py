5d868b869abcd02c054df22903ba98d655857ab1
Bui Thanh Son <buison559@gmail.com>
2019-12-04 00:50:40 +0700

    V3 as main model

--- back_end/object_detection/objectdetector.py
+++ back_end/object_detection/objectdetector.py
@@ -6,8 +6,8 @@ import cv2
 # python real_time_object_detection.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
 # rtsp://admin:1234qwer@192.168.1.4:554/onvif1
 class ObjectDetector:
-  def __init__(self, model='frozen_inference_graph_v2_40000.pb',
-    graph='output_v2.pbtxt'):
+  def __init__(self, model='frozen_inference_graph_v3_59288.pb',
+    graph='output_v3.pbtxt'):
     self.net = cv2.dnn.readNetFromTensorflow(model, graph)
 
   def detect(self, blob):
